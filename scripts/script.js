4ad4a115fb722044d25f7bc3f77cc4ee2b974709
farshid pashazadeh <pashazadeh.freelancer@gmail.com>
2023-07-28 16:15:43 +0330

    calculator with nan output when clicked on equal button

---
+++ scripts/script.js
@@ -0,0 +1,73 @@
+let current_result = "";
+let setnum;
+let setopr;
+let status = {
+    numberOne: "", numberTwo: "", operator: false, currentResult: ""
+};
+let numbers = document.getElementsByClassName("calculator_numbers");
+let operators = document.getElementsByClassName("calculator_operators");
+let resultBox = document.getElementById("result");
+let equal_ele = document.querySelector(".calculator_equal");
+let clear = document.querySelector(".calculator_clear");
+
+// let history = document.getElementById("history");
+
+function setNumber() {
+    for (let number of numbers) {
+        number.addEventListener("click", (e) => {
+            let num = e.target.value;
+            status.currentResult += num;
+            showResult(status.currentResult);
+        });
+    }
+}
+
+setNumber();
+
+for (let operator of operators) {
+    operator.addEventListener("click", (event) => {
+        let opr = event.target.value;
+        setopr = opr;
+        showResult(setopr);
+        status.numberOne = status.currentResult;
+        status.currentResult = "";
+    });
+}
+
+equal_ele.addEventListener("click", () => {
+    status.numberTwo = status.currentResult;
+    status.currentResult = "";
+    calc(status.numberOne, status.numberTwo);
+});
+
+function calc(num1, num2) {
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+    if (setopr === "+") {
+        status.currentResult = num1 + num2;
+    } else if (setopr === "−") {
+        status.currentResult = num1 - num2;
+    } else if (setopr === "×") {
+        status.currentResult = num1 * num2;
+    } else {
+        status.currentResult = num1 / num2;
+    }
+    showResult(status.currentResult);
+    status.currentResult = "";
+}
+
+function showResult(x) {
+    resultBox.value = x;
+}
+
+clear.addEventListener("click",(e)=>{
+    status.numberOne = "";
+    status.numberTwo = "";
+    status.operator = false;
+    status.currentResult = "";
+    showResult("");
+});
+
+
+
+
